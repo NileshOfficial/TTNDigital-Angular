60d8c29977b596618ba438fda3e83d5953f3ad6c
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-22 19:52:28 +0530

    loaders completed

--- src/app/loader/loader.component.ts
+++ src/app/loader/loader.component.ts
@@ -6,9 +6,27 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./loader.component.css']
 })
 export class LoaderComponent implements OnInit {
+  
+  @Input() animation: string;
+  @Input() outerDimension: number;
+  @Input() innerDimension: number;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  setOuterHeightWidth() {
+    return {
+      'height.px': this.outerDimension,
+      'width.px': this.outerDimension
+    }
+  }
+
+  setInnerHeightWidth() {
+    return {
+      'height.px': this.innerDimension,
+      'width.px': this.innerDimension
+    }
+  }
 }
