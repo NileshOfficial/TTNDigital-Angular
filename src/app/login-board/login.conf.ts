22d3943ded32b64b7683a2d21d0fc65ac4a37982
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-22 16:58:57 +0530

    adding login config

---
+++ src/app/login-board/login.conf.ts
@@ -0,0 +1,10 @@
+export const clientId = '1092418005405-igjkksf5ap5vit1d6grhsolnnnu3sd3h.apps.googleusercontent.com';
+export const oauthCodeUri = 'https://accounts.google.com/o/oauth2/v2/auth';
+export const redirect_uri = 'http://localhost:4200/auth';
+export const scope = [
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile',
+];
+export const response_type = 'code';
+export const access_type = 'offline';
+export const prompt = 'consent';
