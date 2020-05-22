74a600673c3e08e20487de4bd9973c9d93f7d3a5
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-23 00:32:49 +0530

    imported icons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { faSignOutAlt, faAngleRight, IconDefinition } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'ttnd-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['../styles/generic.styles.css', './home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  signOutIcon: IconDefinition = faSignOutAlt;
+  rightArrowHead: IconDefinition = faAngleRight;
 
   constructor() { }
 
