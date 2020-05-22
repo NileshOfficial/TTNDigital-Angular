e70f5f573c74938899b5e1227ea4a1338532dc46
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-22 20:21:36 +0530

    auth-callback layout complete

--- src/app/auth-callback/auth-callback.component.ts
+++ src/app/auth-callback/auth-callback.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'ttnd-auth-callback',
   templateUrl: './auth-callback.component.html',
-  styleUrls: ['./auth-callback.component.css']
+  styleUrls: ['../styles/generic.styles.css', './auth-callback.component.css']
 })
 export class AuthCallbackComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    if(!this.activatedRoute.snapshot.queryParams['code'])
+      this.router.navigate(['/', 'auth', 'login']);
   }
 
 }
