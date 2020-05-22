41433c2749dea73f442eda2b6a91be191fe06824
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-23 02:45:56 +0530

    dropdown inputs and html layout complete

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { faAngleUp, faAngleDown, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'ttnd-dropdown',
   templateUrl: './dropdown.component.html',
-  styleUrls: ['./dropdown.component.css']
+  styleUrls: ['../styles/generic.styles.css', './dropdown.component.css']
 })
 export class DropdownComponent implements OnInit {
+
+  @Input() heading: string = '';
+  @Input() options: Array<string | number> = [];
+  @Output() selected: EventEmitter<string | number> = new EventEmitter();
+
+  upArrowHead: IconDefinition = faAngleUp;
+  downArrowHead: IconDefinition = faAngleDown;
+
+  optionsVisible: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleOptions() {
+    this.optionsVisible = !this.optionsVisible;
+  }
+
+  updateHeading(heading: string) {
+    this.heading = heading;
+    this.selected.emit(this.heading);
+  }
 }
