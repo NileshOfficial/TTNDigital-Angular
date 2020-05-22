0fa840c88bd38a449c0990ba6a65297d78465c9d
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-22 20:20:58 +0530

    added auth routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginBoardComponent } from './login-board/login-board.component';
+import { AuthCallbackComponent } from './auth-callback/auth-callback.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
+  {
+    path: 'auth', children: [
+      { path: '', component: AuthCallbackComponent },
+      { path: 'login', component: LoginBoardComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
