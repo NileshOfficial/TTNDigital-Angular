00f4f52e0bac17165865349bd10e1115aac0b0e0
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-23 14:18:54 +0530

    buzz-post init

---
+++ src/app/buzz-post/buzz-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ttnd-buzz-post',
+  templateUrl: './buzz-post.component.html',
+  styleUrls: ['./buzz-post.component.css']
+})
+export class BuzzPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
