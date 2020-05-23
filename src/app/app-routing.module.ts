2aeeaa9caa29858e665ca5e14b5318002fc2a560
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-23 16:42:53 +0530

    hooked buzz with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginBoardComponent } from './login-board/login-board.component';
 import { AuthCallbackComponent } from './auth-callback/auth-callback.component';
+import { BuzzComponent } from './buzz/buzz.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
@@ -10,6 +12,12 @@ const routes: Routes = [
     path: 'auth', children: [
       { path: '', component: AuthCallbackComponent },
       { path: 'login', component: LoginBoardComponent }
+    ]
+  },
+  { path: 'home', redirectTo: '/home/buzz' },
+  {
+    path: 'home', component: HomeComponent, children: [
+      { path: 'buzz', component: BuzzComponent }
     ]
   }
 ];
--- src/app/buzz/buzz.component.ts
+++ src/app/buzz/buzz.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { faPen, faChevronRight, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { faPen, faChevronRight, faAt, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { faImage } from '@fortawesome/free-regular-svg-icons';
+import { Icon } from '@fortawesome/fontawesome-svg-core';
 
 
 @Component({
@@ -13,6 +14,17 @@ export class BuzzComponent implements OnInit {
   penIcon: IconDefinition = faPen;
   postIcon: IconDefinition = faChevronRight;
   imageIcon: IconDefinition = faImage;
+  atIcon: IconDefinition = faAt;
+
+  data = {
+    description: "fksfkhgsfhsgfhjds skgfkhdsf gjhdshk gdsfhkgdsgfhdsg hjdsfgkdsfuerh flkdshflkdsdhfkjl hfdljkhdfkjlghkdsjfghljkfdshglk jdshgldsfh gldfhgjk hdkghhdsjk g kds kg hhkjghdsjklg hdskljghljkds hhglsdjkf gh lkdffsglkd gkjldsfhgljk dshg",
+    category: 'fsdfsdf',
+    images: ['fsdfdsf'],
+    likes: 234,
+    dislikes: 23,
+    date: Date.now(),
+    email: "nilesh.kumar@tothenew.com"
+  }
 
   constructor() { }
 
