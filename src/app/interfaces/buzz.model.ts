82618fd22225359ddf7271ad0c8ea071822573b2
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-23 16:28:44 +0530

    buzz post interface

---
+++ src/app/interfaces/buzz.model.ts
@@ -0,0 +1,9 @@
+export interface buzz {
+    description: string;
+    category: string;
+    images: Array<string>;
+    likes: number;
+    dislikes: number;
+    date: number;
+    email: string;
+};
