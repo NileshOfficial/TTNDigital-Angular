f72f46e637003c443671bc5e218ea881c7625251
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-27 00:30:12 +0530

    admin status is checked before the user is redirected to home on login

--- src/app/auth-callback/auth-callback.component.ts
+++ src/app/auth-callback/auth-callback.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AuthApiService } from '../services/auth-api.service';
 import { LocalstorageService } from '../services/localstorage.service';
+import { TokenstoreService } from '../services/tokenstore.service';
 
 @Component({
   selector: 'ttnd-auth-callback',
@@ -10,17 +11,29 @@ import { LocalstorageService } from '../services/localstorage.service';
 })
 export class AuthCallbackComponent implements OnInit {
 
-  constructor(private authApi: AuthApiService, 
-    private router: Router, 
+  constructor(private authApi: AuthApiService,
+    private router: Router,
     private currentPoint: ActivatedRoute,
-    private localStorage: LocalstorageService) { }
+    private localStorage: LocalstorageService,
+    private tokenstore: TokenstoreService) { }
 
   ngOnInit(): void {
     const code = this.currentPoint.snapshot.queryParams['code'];
     this.authApi.getAuthToken(code).subscribe(token => {
-      this.localStorage.storeToken(token);
-      this.router.navigate(['/home']);
+
+      this.authApi.isAdmin(token.access_token, token.id_token).subscribe(result => {
+        token['admin'] = result.admin;
+        
+        this.tokenstore.token = token;
+        this.localStorage.storeToken(token);
+        this.router.navigate(['/home']);
+      }, err => {
+        console.log(err);
+        this.router.navigate(['/']);
+      });
+
     }, err => {
+      console.log(err);
       this.router.navigate(['/']);
     });
   }
