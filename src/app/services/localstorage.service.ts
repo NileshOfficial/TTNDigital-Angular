b4a1a6821f75ba6ee3be8130e8ad5293f7e44444
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-26 17:34:59 +0530

    methods to store and retrieve token from local storage

--- src/app/services/localstorage.service.ts
+++ src/app/services/localstorage.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { LoginToken } from './auth.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,13 @@ import { Injectable } from '@angular/core';
 export class LocalstorageService {
 
   constructor() { }
+
+  storeToken(token: LoginToken): void {
+    localStorage.setItem('token', JSON.stringify(token));
+  }
+
+  retrieveToken(): LoginToken | null {
+    const token = localStorage.getItem('token');
+    return token ? JSON.parse(token) : null;
+  }
 }
