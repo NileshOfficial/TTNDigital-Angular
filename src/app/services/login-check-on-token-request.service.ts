d4508bb844fbf7c9acb0282f4609b41690f4e2ff
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-26 15:17:03 +0530

    bugfix

--- src/app/services/login-check-on-token-request.service.ts
+++ src/app/services/login-check-on-token-request.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class LoginCheckOnTokenRequestService {
+export class LoginCheckOnTokenRequestService implements CanActivate{
 
   constructor(private router: Router) { }
 
