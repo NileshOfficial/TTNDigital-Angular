114ae9705d3df0cb8e091e518769dbccb32e0984
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-26 17:45:42 +0530

    redirection after checking token on localstorage complete

--- src/app/services/login-check-point.service.ts
+++ src/app/services/login-check-point.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+import { LocalstorageService } from './localstorage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginCheckPointService implements CanActivate {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private localstorage: LocalstorageService) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+    const token = this.localstorage.retrieveToken();
+    console.log(token);
+    if(token && token.access_token) {
+      return true;
+    }
     this.router.navigate(['/']);
     return false;
   }
