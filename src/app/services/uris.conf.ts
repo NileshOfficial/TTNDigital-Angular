af5f30d7009f7e25d18522505b1b97a6e0353184
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-26 19:40:22 +0530

    added refreshAuthToken method

--- src/app/services/uris.conf.ts
+++ src/app/services/uris.conf.ts
@@ -1,1 +1,2 @@
 export const loginRequestEndpoint = 'http://localhost:3001/auth/login';
+export const refreshTokenEndpoint = 'http://localhost:3001/auth/refreshAuthToken';
--- src/app/services/util.service.ts
+++ src/app/services/util.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { LocalstorageService } from './localstorage.service';
+import { refreshTokenEndpoint } from './uris.conf';
+import { LoginToken } from './auth.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UtilService {
 
-  constructor() { }
+  constructor(private http: HttpClient,
+    private localstorage: LocalstorageService,
+    private router: Router) { }
+
+  async refreshAuthToken(callback?: Function, args: any[] = []) {
+    const token = this.localstorage.retrieveToken();
+    if (token && token.refresh_token) {
+      try {
+        const body = {
+          refreshToken: token.refresh_token
+        };
+        const newToken = await this.http.post<LoginToken>(refreshTokenEndpoint, body).toPromise();
+        this.localstorage.storeToken(newToken);
+        if (callback && args)
+          callback(...args);
+      } catch (err) {
+        this.router.navigate(['/']);
+      }
+    }
+  }
 }
