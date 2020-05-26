3e77e5ba1d91c05b3d71c303ba9573c401bc8c9b
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-26 20:37:34 +0530

    added logout

--- src/app/services/auth-api.service.ts
+++ src/app/services/auth-api.service.ts
@@ -3,16 +3,25 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import * as endpoints from './uris.conf';
 import { LoginToken } from './auth.model';
+import { TokenstoreService } from './tokenstore.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthApiService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private tokenstore: TokenstoreService) { }
 
   getAuthToken(code: string): Observable<LoginToken> {
     const endpoint = endpoints.loginRequestEndpoint + '/' + encodeURIComponent(code);
     return this.http.get<LoginToken>(endpoint);
+  }
+
+  logout() {
+    const body = {
+      refreshToken: this.tokenstore.token.refresh_token
+    }
+    return this.http.post(endpoints.logoutEndpoint, body);
   }
 }
--- src/app/services/uris.conf.ts
+++ src/app/services/uris.conf.ts
@@ -1,2 +1,3 @@
 export const loginRequestEndpoint = 'http://localhost:3001/auth/login';
 export const refreshTokenEndpoint = 'http://localhost:3001/auth/refreshAuthToken';
+export const logoutEndpoint = 'http://localhost:3001/auth/logout';
