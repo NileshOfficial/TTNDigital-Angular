f5080e668b3bdae71911e51f707fa903f50e5e8c
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-27 21:26:29 +0530

    added getBuzzFeed

--- src/app/services/buzz-api.service.ts
+++ src/app/services/buzz-api.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import * as endpoints from './uris.conf';
 import { Observable } from 'rxjs';
 import { TokenstoreService } from './tokenstore.service';
+import { buzz } from '../interfaces/buzz.model';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +19,16 @@ export class BuzzApiService {
       'authorization': `bearer ${token.access_token},bearer ${token.id_token}`
     });
     return this.http.post(endpoints.buzzPostEndpoint, formData, {
+      headers: headers
+    });
+  }
+
+  getBuzzFeed(): Observable<Array<buzz>> {
+    const token = this.tokenstore.token
+    let headers = new HttpHeaders({
+      'authorization': `bearer ${token.access_token},bearer ${token.id_token}`
+    });
+    return this.http.get<Array<buzz>>(endpoints.buzzPostEndpoint, {
       headers: headers
     });
   }
