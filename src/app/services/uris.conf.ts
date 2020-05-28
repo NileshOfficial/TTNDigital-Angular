d5f3820862860fa4359d6b1d8c970172ff1c726e
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-28 13:40:51 +0530

    added imagesEndpoint

--- src/app/services/uris.conf.ts
+++ src/app/services/uris.conf.ts
@@ -5,3 +5,4 @@ export const adminCheckEndpoint = 'http://localhost:3001/admin/isAdmin';
 export const buzzPostEndpoint = 'http://localhost:3001/buzz';
 export const updateLikeEndpoint = 'http://localhost:3001/buzz/like';
 export const updateDislikeEndpoint = 'http://localhost:3001/buzz/dislike';
+export const imagesEndpoint = 'http://localhost:3001/images';
