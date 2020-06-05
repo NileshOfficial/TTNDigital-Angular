c5f5bc09c96be1fc3503956b81006ce488db6f25
NileshOfficial <nilesh.kumar@tothenew.com>
2020-06-06 01:25:41 +0530

    added refresh token and admin status from existing token

--- src/app/services/util.service.ts
+++ src/app/services/util.service.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { LocalstorageService } from './localstorage.service';
 import { refreshTokenEndpoint } from './uris.conf';
 import { LoginToken } from './auth.model';
+import { TokenstoreService } from './tokenstore.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,22 +13,30 @@ export class UtilService {
 
   constructor(private http: HttpClient,
     private localstorage: LocalstorageService,
+    private tokenstore: TokenstoreService,
     private router: Router) { }
 
   async refreshAuthToken(callback?: Function, args: any[] = []) {
     const token = this.localstorage.retrieveToken();
     if (token && token.refresh_token) {
+
       try {
         const body = {
           refreshToken: token.refresh_token
         };
+
         const newToken = await this.http.post<LoginToken>(refreshTokenEndpoint, body).toPromise();
+        newToken['refresh_token'] = token.refresh_token;
+        newToken['admin'] = token.admin;
         this.localstorage.storeToken(newToken);
-        if (callback && args)
+        this.tokenstore.token = newToken;
+
+        if (callback)
           callback(...args);
+
       } catch (err) {
         this.router.navigate(['/']);
       }
-    }
+    } else this.router.navigate(['/']);
   }
 }
