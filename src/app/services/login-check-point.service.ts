dfc598efcc64722288fa12545e10a37011a9d434
NileshOfficial <nilesh.kumar@tothenew.com>
2020-06-07 01:52:40 +0530

    after refreshing the page it now checks for validity of token and refreshes the token if expired

--- src/app/services/login-check-point.service.ts
+++ src/app/services/login-check-point.service.ts
@@ -3,6 +3,11 @@ import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from
 import { Observable } from 'rxjs';
 import { LocalstorageService } from './localstorage.service';
 import { TokenstoreService } from './tokenstore.service';
+import { AuthApiService } from './auth-api.service';
+import { map, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { UtilService } from './util.service';
+import { invalidTokenErr } from '../errCodes.conf';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +16,29 @@ export class LoginCheckPointService implements CanActivate {
 
   constructor(private router: Router,
     private localstorage: LocalstorageService,
-    private tokenStore: TokenstoreService) { }
+    private tokenStore: TokenstoreService,
+    private authApi: AuthApiService,
+    private util: UtilService) { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+  async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
     const token = this.localstorage.retrieveToken();
-    if(token && token.access_token) {
+    if (token && token.access_token) {
       this.tokenStore.token = token;
-      return true;
+    } else {
+      this.router.navigate(['/']);
+      return Promise.resolve(false);
     }
-    this.router.navigate(['/']);
-    return false;
+
+    try {
+      await this.authApi.validateAuthToken().toPromise();
+      return Promise.resolve(true);
+    } catch (err) {
+      if (err.error.errorCode === invalidTokenErr) {
+        const updateResponse = await this.util.refreshAuthToken();
+        if (updateResponse) return Promise.resolve(true);
+      }
+      this.router.navigate(['/']);
+      return Promise.resolve(false);
+    }
   }
 }
